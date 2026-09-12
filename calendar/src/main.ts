85d963b16171505e6c007eaeae7453b86859e9d2
Blank <nshipunoff@yandex.ru>
2026-09-12 13:19:30 +0300

    clear vue

--- calendar/src/main.ts
+++ calendar/src/main.ts
@@ -1,7 +1,13 @@
-import './assets/main.css'
+import '@/shared/styles/_normalize.scss'
+import '@/shared/styles/_fonts.scss'
+import '@/shared/styles/_variables.scss'
+import '@/shared/styles/_utils.scss'
+import '@/shared/styles/_globals.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import { i18n } from './i18n'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
